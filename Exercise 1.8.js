bffe23d57f18a58002cd097421d6e5fdefed8d33
Source Academy User <No public email provided>
2021-08-29 12:24:04 +0100

    Changes made from Source Academy

---
+++ Exercise 1.8.js
@@ -0,0 +1,29 @@
+// Exercise 1.8
+function square(x) {
+    return x * x;
+}
+function is_good_enough(guess,  oldguess) {
+    return  oldguess === guess;
+}
+
+function average(x, y) {
+    return (x + y) / 2;
+}
+
+function improve(guess, x) {
+    return ((x / square(guess)) + (2 * guess)) / 3;
+}
+
+function cubrt_iter(guess, x, oldguess) {
+    return is_good_enough(guess, oldguess)
+           ? guess
+           : cubrt_iter(improve(guess, x), x, guess);
+}
+
+function cubrt(x) {
+    return x === 0
+           ? 0
+           : cubrt_iter(1, x, 0);
+}
+
+cubrt(-10);
