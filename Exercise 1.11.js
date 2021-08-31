4ddaa4c930c3b933f70ff061d5952b3a8f50b1e2
Source Academy User <No public email provided>
2021-08-31 10:28:12 +0100

    Changes made from Source Academy

--- Exercise 1.11.js
+++ Exercise 1.11.js
@@ -1,14 +1,24 @@
 //Exercise 1.11
 
 //function with recursive process
-function f(n) {
+function f_recursive(n) {
     return n < 3
            ? n
            : n >= 3
-           ? f(n - 1) + (2 * f(n - 2)) + (3 * f(n - 3))
+           ? f_recursive(n - 1) + (2 * f_recursive(n - 2)) + (3 * f_recursive(n - 3))
            : 0;
 }
 
-f(6);
+//f_recursive(10);
 
-//function with
+//function with iterative process
+function f_iterative(n) {
+    function f_iter(a, b, c, counter) {
+        return counter === 0
+               ? c
+               : f_iter(a + (2*b) + (3*c), a, b, counter - 1);
+    }
+    return f_iter(2, 1, 0, n);
+}
+
+f_iterative(10);
